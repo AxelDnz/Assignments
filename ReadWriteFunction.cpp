2f4088018b7cb7a147068623301ceb0c893bdd29
AxelDnz <66531614+AxelDnz@users.noreply.github.com>
2023-03-29 17:21:39 +0300

    Add files via upload
    A function that solves Q =(𝑋2+3𝑋𝑌−15)/2𝑦 for x,y input values.

---
+++ ReadWriteFunction.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+double FunctionQ(double x, double y)
+{
+	double valueQ{ (x * x + (3 * x * y) - 15) / (2 * y) };
+
+	return valueQ;
+}
+
+
+int main()
+{
+	std::cout << "Please give input value for 'x': ";
+	double x{};
+	std::cin >> x;
+	std::cout << "Please give input value for 'y': ";
+	double y{};
+	std::cin >> y;
+
+	std::cout << "The output falue of function Q is: " << FunctionQ(x, y) << '\n';
+
+	return 0;
+}
